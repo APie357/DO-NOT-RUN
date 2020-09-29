69ff9420a5482b1a9b8b334ede559c862a00f7ed
Genius Format <71953979+Genius-Format@users.noreply.github.com>
2020-09-29 10:00:12 -0700

    Add files via upload

---
+++ DO_NOT_RUN.py
@@ -0,0 +1,20 @@
+# DO_NOT_RUN.py
+# v. 1.0
+# Includes DesktopGoose 0.3
+# https://samperson.itch.io/desktop-goose
+
+import os, random
+from tkinter import *
+from tkinter import messagebox
+
+def exit():
+	time.sleep(3)
+	quit()
+
+messagebox.showwarning("TROLLED!", "HONK HONK, YOU have been TROLLED!!!")
+
+os.system("cd goose && GooseDesktop.exe")
+
+exit()
+
+mainloop()
